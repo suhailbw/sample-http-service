7b416b47ede5003cf4dad5a6488a3bee8843d642
Suhail Gupta <suhail.gupta@betterworks.com>
2021-08-19 18:06:42 +0530

    p

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+console.log(port, "..port");
+
 
 app.get("/", (req, resp) => {
 	const env = require("fs").readFileSync("env.txt",{"encoding": "utf8"});
